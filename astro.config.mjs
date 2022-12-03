ad83b7801bf9934621a21d649ae280610495df08
Douglas Lopes <dl.aguiar@yahoo.com.br>
2022-12-03 19:22:14 -0300

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://dougkusanagi.github.io',
-	base: '/docs',
+	base: '/website',
 	outDir: './docs'
 });
